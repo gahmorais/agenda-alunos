f3d68fe89c6290a4943f6cfe388409e5f0ce26a9
Gabriel Morais <55956559+gahmorais@users.noreply.github.com>
2020-12-12 19:46:48 -0300

    Adicionado activity de adição de aluno

---
+++ app/src/main/java/br/com/gabrielmorais/agendaaluno/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,15 @@
+package br.com.gabrielmorais.agendaaluno.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import br.com.gabrielmorais.agendaaluno.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
--- app/src/main/java/br/com/gabrielmorais/agendaaluno/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/gabrielmorais/agendaaluno/ui/activity/ListaAlunosActivity.java
@@ -1,4 +1,6 @@
-package br.com.gabrielmorais.agendaaluno;/*
+package br.com.gabrielmorais.agendaaluno.ui.activity;
+
+/*
  **
  **
  **  Create by
@@ -9,28 +11,29 @@ package br.com.gabrielmorais.agendaaluno;/*
  **
  */
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearSmoothScroller;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+import br.com.gabrielmorais.agendaaluno.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
+
+        setTitle("Lista de alunos");
+
         List<String> aluno = new ArrayList<>( Arrays.asList( "Alex", "Gabriel", "Ana" ) );
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
 
         listaDeAlunos.setAdapter(new ArrayAdapter<>( this , android.R.layout.simple_list_item_1 , aluno ) );
 
