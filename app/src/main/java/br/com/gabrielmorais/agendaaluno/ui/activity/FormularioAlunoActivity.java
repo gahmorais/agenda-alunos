472bf83348743c2d2dcf403ce6e815b0abb9a4f7
Gabriel Morais <55956559+gahmorais@users.noreply.github.com>
2020-12-12 20:07:45 -0300

    Adicionado campos no formulario e botao salvar

--- app/src/main/java/br/com/gabrielmorais/agendaaluno/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/gabrielmorais/agendaaluno/ui/activity/FormularioAlunoActivity.java
@@ -3,6 +3,9 @@ package br.com.gabrielmorais.agendaaluno.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import br.com.gabrielmorais.agendaaluno.R;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
@@ -11,5 +14,7 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        Button botao_salvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
     }
 }
